8c2d7d1a75b65f84029c5146ef8db2eb40f3ef45
QHL <qhl@lbx.dev>
2021-05-28 16:57:18 +0800

    add game v3

--- gameV3/api/game.js
+++ gameV3/api/game.js
@@ -1,49 +1,60 @@
-const http = require('http')
-const url = require('url')
-const fs = require('fs')
-const queryString = require('querystring')
-const game = require('../lib/game')
 let winCount = 0
 let userLastAction = null
 let userSameAction = 0
-http.createServer(function (req, res) {
-    const parseUrl = url.parse(req.url)
-    if (parseUrl.pathname === '/favicon.ico') {
-        res.writeHead(200)
-        res.end()
-        return
+
+const express = require('express')
+const fs = require('fs')
+const game = require('../lib/game')
+
+const app = express()
+app.get('/favicon.ico', (req, res) => {
+    res.writeHead(200)
+    res.end()
+})
+app.get('/', ((req, res) => {
+    fs.createReadStream(__dirname + '/index.html').pipe(res)
+}))
+app.get('/game', (req, res, next) => {
+    if (winCount > 2) {
+        res.writeHead(500)
+        res.end("500")
     }
-    if (parseUrl.pathname === '/') {
-        fs.createReadStream('../index.html').pipe(res)
+    next()
+    if (res.win) {
+        winCount++
+    } else {
+        winCount = 0
     }
-    if (parseUrl.pathname === '/game') {
-        const query = queryString.parse((parseUrl.query))
-        const playerAction = query.action
-        const result = game(playerAction)
-        if (winCount > 2) {
-            res.writeHead(500)
-            res.end("500")
-        }
-        if (userLastAction === playerAction) {
-            userSameAction++
-        } else {
-            userSameAction = 0
-        }
-        if (userSameAction > 2) {
-            res.writeHead(400)
-            res.end("400")
-        }
-        userLastAction = playerAction
-        res.writeHead(200)
-        if (result === -1) {
-            res.end("你输了")
-            winCount = 0
-        } else if (result === 0) {
-            winCount = 0
-            res.end("平局")
-        } else {
-            winCount++
-            res.end("你赢了")
-        }
+}, (req, res, next) => {
+    const query = req.query
+    const playerAction = query.action
+
+    if (userLastAction === playerAction) {
+        userSameAction++
+    } else {
+        userSameAction = 0
     }
-}).listen(3000)
+    if (userSameAction > 3) {
+        res.writeHead(400)
+        winCount = 8
+        res.end("400")
+    }
+    userLastAction = playerAction
+    res.playerAction = playerAction
+    next()
+
+}, function (req, res) {
+    const playerAction = res.playerAction
+    const result = game(playerAction)
+    res.writeHead(200)
+    if (result === -1) {
+        res.end("你输了")
+    } else if (result === 0) {
+        res.end("平局")
+    } else {
+        res.win = true
+        res.end("你赢了")
+    }
+})
+app.listen(3000)
+
